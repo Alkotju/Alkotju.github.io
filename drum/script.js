cc8277fd2bf53af73724116c5948f07ad46b4300
Alkotju <144673299+Alkotju@users.noreply.github.com>
2025-09-05 11:28:30 +0300

    Add files via upload

---
+++ drum/script.js
@@ -0,0 +1,25 @@
+// проверим готовность документа
+$(function() {
+    //отлавливаем событие "нажатие на клавишу"
+    $(this).keydown(function(event) {
+        //создаем переменную, в которую помещаем div с подходящщим data-key
+        var key = $(this).find('.key[data-key='+event.which+']');
+        //находим на странице тег audio с нужным  data-key и помещаем также в переменную для удобства
+        var audio = $(this).find('audio[data-key='+event.which+']')[0];
+        //присваиваем активный класс к клавише, чтобы подстветить ее
+        key.toggleClass('playing');
+        //проверяем существует ли audio тег с таким data-key
+        if (!audio) return;
+        //воспроизводим звук
+        audio.play();
+        // сбрасываем таймер звуковой дорожки снова на 0
+        audio.currentTime = 0;
+    });
+    //отслеживаем событие когда пользователь отпускает кнопку
+    $(this).keyup(function(event) {
+        // снова создаем переменную с нужным data-key
+        var key = $(this).find('.key[data-key='+event.which+']');
+        // убираем класс который подсвечивает кнопку
+        key.toggleClass('playing');
+    });
+});
