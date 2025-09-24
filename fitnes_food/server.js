e7f0f39427b18b5583779aa0ed036672d1481dcc
Aleksandra Kotju�inskaja <aleksandra.kotjuzinskaja@ivkhk.ee>
2025-09-24 08:59:27 +0300

    proekt fitnes server js

---
+++ fitnes_food/server.js
@@ -0,0 +1,83 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const fs = require('fs');
+
+const app = express();
+const port = 3001;
+
+//Middlewares
+app.use(cors());
+app.use(express.json());
+
+//mongoDB connection
+const mongoURI = 'mongo+srv://alkotju:<895122>@cluster0.sp4bqqv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+mongoose.connect(mongoURI)
+.then(() => console.log('MongoDB connected'))
+.catch(err => console.log(err));
+
+//mongoose schema and model
+
+const menuSchema = new mongoose.Schema({
+    img: String,
+    altimg: String,
+    title: String,
+    descr: String,
+    price: Number
+});
+
+const MenuItem = mongoose.model('MenuItem', menuSchema, 'menu');
+
+const requestSchema = new mongoose.Schema({
+    name: String,
+    phone: String
+});
+
+// function to seed the database
+const seedDatabase = async () => {
+    try {
+        const count = await MenuItem.countDocuments();
+        if (count === 0){
+            console.log('No data found in menu collection. Seeding database...');
+            const data = fs.readFileSync('db.json', 'utf-8');
+            const json = JSON.parse(data);
+            await MenuItem.insertMany(json.menu);
+            console.log('Database seeded successfully.');
+        } else {
+            console.log('Menu collection already contains data. Skipping seed.');
+        }
+    } catch (error) {
+        console.error('Error seeding database:', error);
+    }
+};
+
+const Request = mongoose.model('Request', requestSchema);
+
+//API Routes
+app.post('/request', async (req, res) => {
+    try {
+        const newRequest = new Request(req.body);
+        await newRequest.save();
+        res.status(201).json(newRequest);
+    } catch (error){
+        res.status(400).json({message: error.message });
+    }
+});
+
+app.get('/menu', async (req, res) =>{
+    try{
+        const menuItems = await MenuItem.find();
+        res.json(menuItems);
+    } catch (error) {
+        res.status(500).json({ message: error.message});
+    }
+});
+
+
+// stsrt the server and seed database
+app.listen(port, () => {
+    console.log(`Server is running on port: ${port}`);
+    seedDatabase();
+});
+
+
