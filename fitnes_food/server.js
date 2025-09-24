71f02e049fd6f7e4e82582691d356a556ecefc0d
Aleksandra Kotju�inskaja <aleksandra.kotjuzinskaja@ivkhk.ee>
2025-09-24 11:06:03 +0300

    proekt fitnes upd admin server

--- fitnes_food/server.js
+++ fitnes_food/server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const fs = require('fs');
+const session = require('express-session');
+const path = require('path');
 
 const app = express();
 const port = 3001;
@@ -9,14 +11,28 @@ const port = 3001;
 //Middlewares
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.join(__dirname)));
 
-//mongoDB connection
+app.use(session({
+    secret: 'secret-key',
+    resave: false,
+    saveUninitialized: true,
+    cookie: {secure: false} // v razrabotke mozno ispolzovat http
+}));
+
+//testovij administrator
+const adminUser = {
+    username: 'admin',
+    password: 'password'
+};
+
+//MongoDB connection
 const mongoURI = 'mongodb+srv://alkotju:895122@cluster0.sp4bqqv.mongodb.net/db?retryWrites=true&w=majority&appName=Cluster0';
 mongoose.connect(mongoURI)
 .then(() => console.log('MongoDB connected'))
 .catch(err => console.log(err));
 
-//mongoose schema and model
+//Mongoose schema and model
 const menuSchema = new mongoose.Schema({
     img: String,
     altimg: String,
@@ -52,6 +68,47 @@ const seedDatabase = async () => {
 
 const Request = mongoose.model('Request', requestSchema);
 
+//Middleware dlja proverki auth 
+const isAuthenticated = (req, res, next) => {
+    if (req.session.user) {
+        next();
+    }else {
+        res.status(401).send('UnauTHORized');
+    }
+};
+
+//Маршрут для входа
+app.post('/login', (req, res) => {
+    const { username, password} = req.body;
+    if (username === adminUser.username && password === adminUser.password){
+        req.session.user = adminUser;
+        res.status(200).send('Login successful');
+    } else {
+        res.status(401).send('Invalid credentials')
+    }
+})
+//Маршрут для выхода
+app.post('/loguot', (req, res) => {
+    req.session.destroy();
+    res.status(200).send('Logout successful');
+});
+
+//Маршрут для проверки статуса сеанса
+app.get('/session-status', (req, res) => {
+    if (req.session.user){
+        res.status(200).json({loggedIn: true});
+    } else {
+        res.status(200).json({loggedIn: false});
+    }
+});
+
+//Маршрут для админ-панели
+app.get('/admin', isAuthenticated, (req, res) =>{
+    res.sendFile(path.join(__dirname, 'admin/admin.html'));
+});
+
+
+
 //API Routes
 app.post('/requests', async (req, res) => {
     try {
@@ -69,6 +126,36 @@ app.get('/menu', async (req, res) =>{
         res.json(menuItems);
     } catch (error) {
         res.status(500).json({ message: error.message});
+    }
+});
+
+app.post('/menu', isAuthenticated, async(req, res) =>{
+    try {
+        const newItem= new MenuItem(req.body);
+        await newItem.save();
+        res.status(201).json(newItem);
+    } catch (error){
+        res.status(400).json({ message: error.message});
+    }
+});
+
+app.delete('/menu/:id', isAuthenticated, async (req, res) =>{
+    try{
+        const { id } = req.params;
+        await MenuItem.findByIdAndDelete(id);
+        res.status(200).send('Menu item deleted');
+    } catch (error) {
+        res.status(400).json({ message: error.message});
+    }
+});
+
+app.put('/menu/:id', isAuthenticated, async (req, res) =>{
+    try{
+        const { id } = req.params;
+        const updatedItem = await MenuItem.findByIdAndUpdate(id, req.body, {new: true});
+        res.status(200).json(updatedItem);
+    } catch (error) {
+        res.status(400).json({ message: error.message});
     }
 });
 
